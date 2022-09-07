6a579576a295623e29895245973dfcac7e8c6c90
xiaotong-zhang <zxt_0310@126.com>
2022-09-07 16:39:01 +0800

    Initial commit

---
+++ src/main/java/cn/xtong/wechat_login/WechatLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.xtong.wechat_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatLoginDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatLoginDemoApplication.class, args);
+    }
+
+}
